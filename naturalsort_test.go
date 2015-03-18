990d8ab76a04c5d486ebf9cb2f047cc52b3c03e4
Ahmet Alp Balkan <ahmetalpbalkan@gmail.com>
2015-03-17 22:33:06 -0700

    more test cases

--- naturalsort_test.go
+++ naturalsort_test.go
@@ -7,13 +7,46 @@ import (
 )
 
 func TestSortValid(t *testing.T) {
-	data := []string{"A1BA1", "A11AA1", "A2AB0", "B1AA1", "A1AA1"}
-	expected := []string{"A1AA1", "A1BA1", "A2AB0", "A11AA1", "B1AA1"}
-
-	sort.Sort(NaturalSort(data))
-	if !reflect.DeepEqual(data, expected) {
-		t.Fatalf("Wrong order.\nExpected=%v\nGot=%v", expected, data)
+	cases := []struct {
+		data, expected []string
+	}{
+		{
+			nil,
+			nil,
+		},
+		{
+			[]string{},
+			[]string{},
+		},
+		{
+			[]string{"a"},
+			[]string{"a"},
+		},
+		{
+			[]string{"0"},
+			[]string{"0"},
+		},
+		{
+			[]string{"1", "2", "30", "22", "0", "00", "3"},
+			[]string{"0", "00", "1", "2", "3", "22", "30"},
+		},
+		{
+			[]string{"A1", "A0", "A21", "A11", "A111", "A2"},
+			[]string{"A0", "A1", "A2", "A11", "A21", "A111"},
+		},
+		{
+			[]string{"A1BA1", "A11AA1", "A2AB0", "B1AA1", "A1AA1"},
+			[]string{"A1AA1", "A1BA1", "A2AB0", "A11AA1", "B1AA1"},
+		},
 	}
+
+	for i, c := range cases {
+		sort.Sort(NaturalSort(c.data))
+		if !reflect.DeepEqual(c.data, c.expected) {
+			t.Fatalf("Wrong order in test case #%d.\nExpected=%v\nGot=%v", i, c.expected, c.data)
+		}
+	}
+
 }
 
 func BenchmarkSort(b *testing.B) {
