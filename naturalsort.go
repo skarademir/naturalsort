4543984a2554b8a3a4fef8546c444bf349bba33c
skarademir <saruhan@MacBook-Pro.local>
2015-04-11 13:35:31 -0700

    Move regexp object to global, Closes #4
    Doubles throughput

--- naturalsort.go
+++ naturalsort.go
@@ -8,6 +8,8 @@ import (
 
 type NaturalSort []string
 
+var r = regexp.MustCompilePOSIX(`[^0-9]+|[0-9]+`)
+
 func (s NaturalSort) Len() int {
 	return len(s)
 }
@@ -15,16 +17,15 @@ func (s NaturalSort) Swap(i, j int) {
 	s[i], s[j] = s[j], s[i]
 }
 func (s NaturalSort) Less(i, j int) bool {
-	r := regexp.MustCompilePOSIX(`[^0-9]+|[0-9]+`)
 
 	spliti := r.FindAllString(strings.Replace(s[i], " ", "", -1), -1)
 	splitj := r.FindAllString(strings.Replace(s[j], " ", "", -1), -1)
-	
+
 	splitshortest := len(spliti)
-	if len(spliti) > len(splitj)  {
+	if len(spliti) > len(splitj) {
 		splitshortest = len(splitj)
 	}
-	for index := 0; index < splitshortest; index ++{
+	for index := 0; index < splitshortest; index++ {
 		if spliti[index] != splitj[index] {
 			inti, ei := strconv.Atoi(spliti[index])
 			intj, ej := strconv.Atoi(splitj[index])
