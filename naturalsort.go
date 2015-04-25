61d52eb580b8c594783bb32a43d3e5b74a6c839d
skarademir <skarademir@gmail.com>
2015-04-24 19:49:23 -0700

    Replaces costly and faulty Atoi  with character based comparison
    Solves issue #10

--- naturalsort.go
+++ naturalsort.go
@@ -2,13 +2,12 @@ package naturalsort
 
 import (
 	"regexp"
-	"strconv"
 	"strings"
 )
 
 type NaturalSort []string
 
-var r = regexp.MustCompilePOSIX(`[^0-9]+|[0-9]+`)
+var r = regexp.MustCompile(`[^0-9]+|[0-9]+`)
 
 func (s NaturalSort) Len() int {
 	return len(s)
@@ -23,15 +22,20 @@ func (s NaturalSort) Less(i, j int) bool {
 
 	for index := 0; index < len(spliti) && index < len(splitj); index++ {
 		if spliti[index] != splitj[index] {
-			// Try to convert the left and right portions to integer
-			inti, erri := strconv.Atoi(spliti[index])
-			intj, errj := strconv.Atoi(splitj[index])
 			// Handle numerical case
-			if erri == nil && errj == nil { //if number
-				if inti == intj {
+			if isNumber(spliti[index]) && isNumber(splitj[index]) { //if number
+				// Remove Leading Zeroes
+				stringi := strings.TrimLeft(spliti[index], "0")
+				stringj := strings.TrimLeft(splitj[index], "0")
+				if len(stringi) == len(stringj) {
+					for indexchar := 0; indexchar < len(stringi); indexchar++ {
+						if stringi[indexchar] != stringj[indexchar] {
+							return stringi[indexchar] < stringj[indexchar]
+						}
+					}
 					return len(spliti[index]) < len(splitj[index])
 				}
-				return inti < intj
+				return len(stringi) < len(stringj)
 			}
 			// Handle non-numerical case
 			return spliti[index] < splitj[index]
@@ -39,4 +43,7 @@ func (s NaturalSort) Less(i, j int) bool {
 
 	}
 	return s[i] < s[j]
+}
+func isNumber(input string) bool {
+	return input[0] >= '0' && input[0] <= '9'
 }
